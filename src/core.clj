c8fc998a9f2d97c1abbfb79dc222d798bf14d6fc
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2025-02-22 20:31:39 -0300

    Add comments to know what is going on

--- src/core.clj
+++ src/core.clj
@@ -1,22 +1,27 @@
 (ns core
   (:require [nrepl.server :refer [start-server]])
+  ;; This tells clojure to generate a Java Class for this module. 
+  ;; This makes java `find` this module.
+  ;; Clojure see java, but java does not see clojure out of the box
   (:gen-class))
 
-;; Just a function to check if i can connect to the running jar and change things on the fly
-(defn can-i-run-this? [] (println "Your are in, and the rest of the code is here"))
-
+;; If you can see this function this mean that you are on the jar running this code.
 ;; Change this function to see the hot-reloading taking place
 (defn printed-every-second [] ".")
 
+(def not-a-fn ".")
+;; This is the main entrypoint of the application.
 (defn -main []
   (println "Starting REPL server on port 7888...")
+  ;; We start the start-server for nrepl, this is non blocking 
   (let [server (start-server :port 7888)]
     (println "REPL server started!")
     (try
+      ;; Infinity Loop that keeps printing what is the function above
       (loop [i 0]
         (if (zero? (rem i 80))
           (println "")
-          (print (printed-every-second)))
+          (print not-a-fn))
         (flush)
         (Thread/sleep 1000)
         (recur (inc i)))
@@ -24,3 +29,6 @@
         (println "Stopping REPL server...")
         ;; Gracefully stop the server when done
         (.close server)))))
+
+(comment
+  (printed-every-second))
